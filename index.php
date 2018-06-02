dbe7a30bfb0c7b8f7249d5fcbf56e8c57e741387
Peace D <djnkuli@gmail.com>
2018-06-02 21:50:20 +0200

    setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Optimus Prime
+ * Date: 2018/06/02
+ * Time: 09:47 PM
+ * Description: This file illustrates how to create, retrieve, update and delete data from a database in PHP
+ * Copyright: This system/file is owned by Peace Dube. Unauthorised copying is prohibited
+ */
+require_once ('includes/database.php');
+
